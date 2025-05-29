1b6397ef65fe637dc44a87834b5a8743b29dd3b5
Vithor <vithornelson435@hotmail.com>
2025-05-29 13:16:47 -0300

    Subindo correções

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:PostgreSQL@localhost:5432/PetResgate"
+DATABASE_URL = "postgresql://postgres:senha@localhost:5432/PetResgate"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- models.py
+++ models.py
@@ -3,24 +3,25 @@ from sqlalchemy.orm import relationship
 from database import Base
 
 class Usuario(Base):
-    __tablename__ = "Usuario"
+    __tablename__ = "usuario"
     id_usuario = Column(Integer, primary_key=True, index=True)
     tipo_usuario = Column(String(20), nullable=False)
     nome = Column(String(50), nullable=False)
     idade = Column(Integer)
     email = Column(String(50), unique=True, nullable=False)
-    CPF = Column(String(11))
-    CNPJ = Column(String(14))
+    cpf = Column(String(11))
+    cnpj = Column(String(14))
     senha = Column(String(50), nullable=False)
     telefone = Column(String(20))
 
-    pets = relationship("Pet", back_populates="dono")
-    adocoes = relationship("Adocao", back_populates="adotante")
+    pets = relationship("Pet", back_populates="dono", cascade="all, delete")
+    adocoes = relationship("Adocao", back_populates="adotante", cascade="all, delete")
+
 
 class Pet(Base):
-    __tablename__ = "Pet"
+    __tablename__ = "pet"
     id_pet = Column(Integer, primary_key=True, index=True)
-    id_usuario = Column(Integer, ForeignKey("Usuario.id_usuario", ondelete="CASCADE"), nullable=False)
+    id_usuario = Column(Integer, ForeignKey("usuario.id_usuario", ondelete="CASCADE"), nullable=False)
     nome = Column(String(20))
     idade = Column(Integer)
     animal = Column(Text)
@@ -29,15 +30,19 @@ class Pet(Base):
     descricao = Column(Text)
     status = Column(String(30))
     data_resgate = Column(Date)
+    brinca = Column(Integer)
+    carinhoso = Column(Integer)
+    sociavel = Column(Integer)
 
     dono = relationship("Usuario", back_populates="pets")
     adocao = relationship("Adocao", uselist=False, back_populates="pet")
 
+
 class Adocao(Base):
-    __tablename__ = "Adocao"
+    __tablename__ = "adocao"
     id_adocao = Column(Integer, primary_key=True, index=True)
-    id_usuario = Column(Integer, ForeignKey("Usuario.id_usuario", ondelete="CASCADE"), nullable=False)
-    id_pet = Column(Integer, ForeignKey("Pet.id_pet", ondelete="CASCADE"), unique=True, nullable=False)
+    id_usuario = Column(Integer, ForeignKey("usuario.id_usuario", ondelete="CASCADE"), nullable=False)
+    id_pet = Column(Integer, ForeignKey("pet.id_pet", ondelete="CASCADE"), unique=True, nullable=False)
     data_adocao = Column(Date, nullable=False)
     formulario = Column(Text)
 
